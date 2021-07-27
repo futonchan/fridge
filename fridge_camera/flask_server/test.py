3d9d3bad407e5dac6e68394bb7ec2d82d9a6e1b9
hiroki <assdroi555@gmail.com>
2021-07-27 22:36:29 +0900

    feat: GETするとDBのデータをJSONで返すテスト用サーバーを追加

--- fridge_camera/flask_server/test.py
+++ fridge_camera/flask_server/test.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///testdb.sqlite"
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'Hello'
+class VegetModel(db.Model):
+    # テーブル名
+    __tablename__ = 'veget_model'
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run() # 指定なしだとローカルでいける
+    # カラム情報
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    # def __init__(self, name, count): # なぜinitでこれはいる？
+    #     self.name = name
+    #     self.count = count
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'count': self.count
+        }
+
+    def __repr__(self): # 確認用にprintしてくれる
+        return f"Veget(name={name}, count={count})"
+
+@app.route("/", methods=["GET"])
+def list_veget():
+    vegets = VegetModel.query.all()
+    return jsonify({'vegets': [veget.to_dict() for veget in vegets]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
